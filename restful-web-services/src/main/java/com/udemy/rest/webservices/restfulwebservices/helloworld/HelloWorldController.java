0412056471cd6a164bfdf93eb4530cc45723c6b2
Jitender Kumar <jitender.kumar@irissoftware.com>
2018-09-19 13:44:10 +0530

    Internalization i18n support added

--- restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,22 @@
 package com.udemy.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	MessageSource messageSource;
 	
 	//GET
 	//URI - /hello
@@ -27,4 +36,13 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean("Hello " + name);
 	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	/*public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}*/
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
+
 }
