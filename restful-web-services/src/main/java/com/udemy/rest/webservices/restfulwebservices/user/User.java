a8bf6fd0b64902b8695a9183ef0c519112e510a5
Jitender Kumar <jitender.kumar@irissoftware.com>
2018-09-19 12:32:56 +0530

    Added code for Validation, Exception handling and HATEOAS

--- restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/user/User.java
@@ -2,11 +2,17 @@ package com.udemy.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import javax.xml.crypto.Data;
 
 public class User {
 	private Integer id;
+	
+	@Size(min=2, message="Name should have at least two characters")
 	private String name;
+	
+	@Past
 	private Date dateOfBirth;
 
 	public User(Integer id, String name, Date dateOfBirth) {
--- restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,25 +1,67 @@
 package com.udemy.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
-	
+
 	@Autowired
 	private UserDaoService service;
-	
-	@GetMapping(path="/users")
+
+	@GetMapping(path = "/users")
 	public List<User> retrieveAllUsers() {
 		return service.findAll();
 	}
-	
-	@GetMapping(path="/users/{id}")
-	public User findUser(@PathVariable int id) {
-		return service.findOne(id);
+
+	@GetMapping(path = "/users/{id}")
+	//public User findUser(@PathVariable int id) {
+	public Resource<User> findUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+		
+		//HATEOAS: retrieve all users
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		
+		return resource;
+		//return user;
+	}
+
+	@PostMapping(path = "/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		User savedUser = service.save(user);
+		// CREATED - /users/{id}
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping(path = "/users/{id}")
+	public void deleteUserById(@PathVariable int id) {
+		User user = service.deleteUser(id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
 	}
 }
