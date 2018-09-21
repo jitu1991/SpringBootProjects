36b632c40ecfc44ebffc1e28567761def1f8ae36
Jitender Kumar <jitender.kumar@irissoftware.com>
2018-09-21 16:48:38 +0530

    Dynamic filtering for REST

--- restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,17 +1,47 @@
 package com.udemy.rest.webservices.restfulwebservices.filtering;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	
-	@GetMapping("/filtering")
+	/*@GetMapping("/filtering")
 	public TestBean retrieveBean() {
 		return new TestBean("value1", "value2", "value3");
+	}*/
+	
+	@GetMapping("filtering")
+	public MappingJacksonValue retrieveBean() {
+		TestBean bean = new TestBean("value1", "value2", "value3");
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("TestBeanFilter", filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(bean);
+		mapping.setFilters(filters);
+		return mapping;
 	}
 	
+	@GetMapping("/filtering-list")
+	public MappingJacksonValue retrieveListOfBeans() {
+		List<TestBean> list = Arrays.asList(new TestBean("value1", "value2", "value3"), new TestBean("value11", "value22", "value33"));
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("TestBeanFilter", filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(list);
+		mapping.setFilters(filters);
+
+		return mapping;
+	}
 }
--- restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/filtering/TestBean.java
+++ restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/filtering/TestBean.java
@@ -1,13 +1,15 @@
 package com.udemy.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //@JsonIgnoreProperties(value= {"field1","field2"})
+@JsonFilter("TestBeanFilter")
 public class TestBean {
 	private String field1;
 	private String field2;
-	@JsonIgnore
+	//@JsonIgnore
 	private String field3;
 
 	public TestBean(String field1, String field2, String field3) {
